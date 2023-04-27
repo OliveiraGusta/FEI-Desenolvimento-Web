26ae74a441be0d7f4d3b023a1594cd8f3b8179e2
OliveiraGusta <gustavo.oliver456@gmail.com>
2023-04-27 01:03:36 -0300

    feat[Node]: Socket.io, comecando projeto de teste de conexao web socket

--- Node/server.js
+++ Node/server.js
@@ -1,34 +1,28 @@
+const http = require('http');
 const express = require('express');
-const path = require('path');
+const socketIO = require('socket.io');
 
 const app = express();
-const server = require('http').createServer(app);
-const io  = require('socket.io')(server);
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+const PORT = process.env.PORT || 3000;
 
-app.use('/', (req, res) => {
-   res.render('index.html');
-});
+app.use(express.static('./public'));
 
-let messages = [];
-
-io.on('connection', socket =>{
-    console.log(`Novo usuario conectado: ${socket.id}`);
-
-
-    socket.emit('previousMessages', messages);
-
-    
-    socket.on('sendMessage', data =>{
-        messages.push(data);
-        socket.broadcast.emit('receivedMessage', data);
-    });
-});
-
-server.listen(80, () => {
-    console.log(`Rodando na porta 80`)
+io.on('connection', (socket) => {
+  console.log('Um usuário conectou');
+  
+  socket.on('mensagem', (mensagem) => {
+    console.log('Mensagem recebida:', mensagem);
+    io.emit('mensagem', mensagem);
   });
+
+  socket.on('disconnect', () => {
+    console.log('Um usuário desconectou');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
