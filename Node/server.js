a944a4763c1baf0bf82564038228121a2ccaa298
OliveiraGusta <gustavo.oliver456@gmail.com>
2023-04-27 18:00:46 -0300

    feat [WebSocket]: criado pagina teste_chat para aplicar teste de uma chat baseado na documentacao da API

--- Node/server.js
+++ Node/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = 80;
 
 app.use(express.static('./public'));
 
@@ -21,7 +21,23 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('Um usuário desconectou');
   });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('nameUser', (nameUser) => {
+    console.log('Nome:',nameUser);
+    io.emit('nameUser', nameUser);
+  });
+
+  socket.on('digitando', (digitando) => {
+    console.log('Usuario esta digitando...' );
+    io.emit('digitando', digitando);
+  });
+
 });
+
 
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
