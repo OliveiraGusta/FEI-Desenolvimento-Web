bc7399139ce1a37a437055feee22ab34f7c7d78d
GUSTAVO RODRIGUES OLIVEIRA <unifgusoliveira@feinet.fei.edu.br>
2023-05-18 22:36:00 -0300

    feat[mongodb] inicio da instalacao mongobd

--- Projeto Principal/server.js
+++ Projeto Principal/server.js
@@ -3,6 +3,13 @@ var express = require('express')
 var app = express()
 var BodyParser = require('body-parser')
 
+var username_db = encodeURIComponent('oliveiragusta_aula');
+var password_db = encodeURIComponent('123');
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${username_db}:${password_db}@cluster0.ezpcx6t.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.use(express.static('./public'))
 app.use(BodyParser.urlencoded({extended: false}))
@@ -27,7 +34,7 @@ app.get('/cadastra', (req, res) => {
     res.redirect('cadastro.html')
 })
 
-app.post('/cadastra', (req, res) => {
+/*app.post('/cadastra', (req, res) => {
     let nome = req.body.name;
     let senha = req.body.pass;
     let usuarioJaExistente = false;
@@ -48,6 +55,18 @@ app.post('/cadastra', (req, res) => {
         res.render('respostaCadastro', {nome, usuarioJaExistente})
     }
    
+});*/
+
+app.post("/cadastra", function(req, resp) {
+   // salva dados no banco
+   client.db("username_db").collection("usuarios").insertOne(
+    { db_nome: req.body.name, db_senha: req.body.pass}, function (err) {
+    if (err) {
+        res.render('respostaCadastro', {nome, true})
+    }else {
+        res.render('respostaCadastro', {nome, false})
+    };
+  });
 });
 
 
